a623c041734dbfeea5c5ec775855fcde40314bcc
Sachintha Waduge <isurusachin123@gmail.com>
2024-09-13 23:42:45 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGODB
+)
+.then(()=> {
+    console.log('MongoDb is connected');
+}).catch((err)=>{
+    console.log(err);
+});
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000,()=> {
+    console.log('Server is running on port 3000!!')
+})
+
+app.use('/api/test',userRoutes);
