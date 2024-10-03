cc1f58daabcf1e37982a31e2e378c70b06297e38
Sachintha Waduge <isurusachin123@gmail.com>
2024-10-03 23:09:11 +0530

    update api proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api':{
-        target: 'http://localhost:8000',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
