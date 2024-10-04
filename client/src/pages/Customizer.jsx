e2af5415679b1eadd9c4e7e0b8740e1c89925d50
Sachintha Waduge <isurusachin123@gmail.com>
2024-10-05 00:23:07 +0530

    change customizer path

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -49,7 +49,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
